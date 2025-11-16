017b6db956bf66e1c07f42450dcaa2b5730a70bd
Caio Salesi <cvsalarc@gmail.com>
2025-11-15 23:26:21 -0300

    docker funcionando

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -6,6 +6,8 @@ exports.getAllPosts = (req, res) => {
 
 exports.createPost = (req, res) => {
     const { id, name, text } = req.body;
+    if (posts.some(item => item.id === id))
+        return res.status(401).json("ID deve ser única");
     const post = { id, name, text };
     posts.push(post)
     res.status(201).json(post);
