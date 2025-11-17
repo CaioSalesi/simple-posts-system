424448d64ad6ad3c5a1f4cd13ca256fa8cd67cfb
caio.s <caio.s@futebolcard.com.br>
2025-11-17 11:52:12 -0300

    adicionando métodos http mais complexos

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,12 +3,49 @@ let posts = [];
 exports.getAllPosts = (req, res) => {
     res.json(posts);
 }
+exports.getPostById = (req, res) => {
+    const id = req.params.id;
+    const post = posts.find(p => p.id === id);
 
+    if (!post)
+        return res.status(404).json("Post não encontrado");
+
+    res.json(post);
+}
 exports.createPost = (req, res) => {
     const { id, name, text } = req.body;
-    if (posts.some(item => item.id === id))
-        return res.status(401).json("ID deve ser única");
+
+    if (posts.some(p => p.id === id))
+        return res.status(400).json("ID deve ser única");
+    if (typeof id !== "string" || typeof name !== "string" || typeof text !== "string")
+        return res.status(400).json("Os todos os dados devem ser passados como String");
+    
     const post = { id, name, text };
     posts.push(post)
+
     res.status(201).json(post);
+}
+exports.updatePostById = (req, res) => {
+    const id = req.params.id;
+    const { name, text } = req.body;
+    const post = posts.find(p => p.id === id);
+
+    if (typeof name !== "string" || typeof text !== "string")
+        return res.status(400).json("Os todos os dados devem ser passados como String");
+    if (!post)
+        return res.status(404).json("Post não encontrado");
+
+    post.name = name;
+    post.text = text;
+    res.json(post);
+}
+exports.deletePostById = (req, res) => {
+    const id = req.params.id;
+    const index = posts.findIndex(p => p.id === id);
+
+    if (index === -1)
+        return res.status(404).send("Post não encontrado")
+
+    posts.splice(index, 1);
+    res.status(204).send();
 }
